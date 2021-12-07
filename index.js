ede5c5b13b6e570199acd4f9289d3e5ef750e296
zcaldwell <mintyst.john@gmail.com>
2021-12-07 10:33:28 -0800

    testing

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ export const getCountryNames = (countries) => {
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
-
+export const getCountryCodes = (countries) => {
+  return countries.map((name) => name.name && name.iso2);
+};
+  
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries in Oceana
 // REQS: use .filter
